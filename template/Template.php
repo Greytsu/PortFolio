36d031bf8223ce28d538d0eec9de55df4c220fc7
Olivier <olivier.coujan@gmail.com>
2020-01-23 16:18:55 +0100

    Footer

--- template/Template.php
+++ template/Template.php
@@ -6,25 +6,84 @@
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 </head>
 
-<nav class="navbar navbar-expand-lg sticky-top container border-left border-right border-bottom" style="background-color: #15202B;">
+<nav class="navbar navbar-expand-lg centre border-left border-right border-bottom rounded-bottom" style="background-color: #15202B;">
 	<a class="navbar-brand brand" href="?page=home">Accueil</a>
     <div class="navbar-nav">
       <a class="nav-item nav-link onglet" href="?page=formation">Formations</a>
       <a class="nav-item nav-link onglet" href="?page=VeilleInfo">Veille informatique</a>
     </div>
 </nav>
-
+<br>
 
 <body>
-	<div class='container border-left border-right border-bottom rounded-bottom'>
-
+	<div class='centre border-left border-right border-bottom border-top rounded-bottom rounded-top'>
+			<br>
 			<?php echo $content; ?>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
 	</div>
 </body>
 
+<br>
 
-<footer>
+<footer class='centre border-left border-right border-top rounded-top'>
 	
+	Test
 
 </footer>
 
